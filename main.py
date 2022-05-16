45eb5f2bb562a0c8c49de18b3f873340392c7b09
Saurav Sudhakar <sauravsudhakar@gmail.com>
2022-05-16 18:56:01 +0530

    removed comments

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
         ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
         ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]]
  
-        #keys list stores the button of the keyboard
+        
 
         
 finalText = "" 
